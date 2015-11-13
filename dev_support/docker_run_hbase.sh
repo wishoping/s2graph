8f7f9e5d447e0e1b52c23523d8ff57a23bd62b91
Jaesang Kim <honeysleep@gmail.com>
2015-11-13 13:42:21 +0900

    change run script

--- dev_support/docker_run_hbase.sh
+++ dev_support/docker_run_hbase.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run --name graph_hbase -h docker-box -p 2181:2181 -p 60010:60010 -p 60000:60000 -p 60020:60020 -p 60030:60030 -d nerdammer/hbase:0.98.10.1
+docker run \
+	--name graph_hbase \
+	-p 2181:2181 \
+	-p 60010:60010 \
+	-p 60000:60000 \
+	-p 60020:60020 \
+	-p 60030:60030 \
+	--net host \
+	-d \
+	nerdammer/hbase:0.98.10.1
--- dev_support/docker_run_mysql.sh
+++ dev_support/docker_run_mysql.sh
@@ -4,6 +4,7 @@ docker run \
 	--name graph_mysql \
 	-e MYSQL_ROOT_PASSWORD=graph \
 	-p 3306:3306\
+	--net host \
 	-d \
 	graph_mysql
 
